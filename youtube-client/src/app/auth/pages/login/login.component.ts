ce5b18bccaef07232426c5bc0dbb6a2428f6291c
Alina Husarava <alinagusarik@gmail.com>
2022-04-25 11:41:53 +0300

    feat: task forms complete

--- youtube-client/src/app/auth/pages/login/login.component.ts
+++ youtube-client/src/app/auth/pages/login/login.component.ts
@@ -2,23 +2,43 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { TOKEN } from './../../../constants';
 import { createToken } from 'src/app/shared/utils';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { regExValidator } from './../../validation';
+import { PASSWORD_REG_EX } from './../../../constants';
+import { ILoginForm } from '../../models/login.model';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private fb: FormBuilder) {}
 
-  public regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/;
+  public loginFields: ILoginForm[] = [
+    {
+      id: 'name',
+      formControlName: 'name',
+      type: 'text',
+      messageError: {
+        email: 'Please enter a valid email: example@email.com',
+        required: 'Please enter a login email',
+      },
+    },
+    {
+      id: 'password',
+      formControlName: 'password',
+      type: 'password',
+      messageError: {
+        regEx:
+          'Your password must be have at least 8 characters long, 1 uppercase, 1 lowercase character, 1 number, 1 special character, e.g., ! @ # ? ]',
+        required: 'Please enter a password',
+      },
+    },
+  ];
 
-  public login = new FormGroup({
-    name: new FormControl(null, [Validators.required, Validators.email]),
-    password: new FormControl(
-      null,
-      Validators.compose([Validators.required, Validators.pattern(this.regex)]),
-    ),
+  public login = this.fb.group({
+    name: [null, [Validators.required, Validators.email]],
+    password: [null, [Validators.required, regExValidator(PASSWORD_REG_EX)]],
   });
 
   userLogin() {
@@ -27,7 +47,24 @@ export class LoginComponent {
     this.router.navigate(['/']);
   }
 
-  info() {
-    console.log(this.login.get('name')?.errors?.['email']);
+
+
+  createErrorMessage(loginField: ILoginForm): string | undefined {
+    let message: string | undefined;
+    switch (true) {
+      case !!this.login?.get(loginField.id)?.errors?.['required']:
+        message = loginField.messageError.required;
+        break;
+      case !!this.login?.get(loginField.id)?.errors?.['email']:
+        message = loginField.messageError.email;
+        break;
+
+      case !!this.login?.get(loginField.id)?.errors?.['regEx']:
+        message = loginField.messageError.regEx;
+        break;
+      default:
+        message = '';
+    }
+    return message;
   }
 }
--- youtube-client/src/app/youtube/pages/create-card/create-card.component.ts
+++ youtube-client/src/app/youtube/pages/create-card/create-card.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
+import { regExValidator } from 'src/app/auth/validation';
+import { IAdminForm } from '../../models/form.model';
+import { URL_REG_EX } from './../../../constants';
+import { DATE_REG_EX } from './../../../constants';
 
 @Component({
   selector: 'app-create-card',
@@ -7,14 +11,80 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-card.component.scss'],
 })
 export class CreateCardComponent {
-  public newCard = new FormGroup({
-    title: new FormControl(null, Validators.required),
-    description: new FormControl(null, Validators.required),
-    img: new FormControl(null, Validators.required),
-    link: new FormControl(null, Validators.required),
+  constructor(public fb: FormBuilder) {}
+
+  public fields: IAdminForm[] = [
+    {
+      id: 'title',
+      formControlName: 'title',
+      messageError: {
+        minLength: 'The title is too short',
+        maxLength: 'The title is too long',
+        required: 'Please enter a title',
+      },
+    },
+    {
+      id: 'description',
+      formControlName: 'description',
+      messageError: {
+        maxLength: 'The description is too long',
+      },
+    },
+    {
+      id: 'img',
+      formControlName: 'img',
+      messageError: {
+        pattern: 'The image link is invalid',
+        required: 'Please enter a link to the image',
+      },
+    },
+    {
+      id: 'link',
+      formControlName: 'link',
+      messageError: {
+        pattern: 'The video link is invalid',
+        required: 'Please enter a link to the video',
+      },
+    },
+    {
+      id: 'date',
+      formControlName: 'date',
+      messageError: {
+        pattern: 'The date is invalid, format: DD/MM/YYYY',
+        required: 'Please enter a creation date',
+      },
+    },
+  ];
+
+  public newCard = this.fb.group({
+    title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
+    description: ['', Validators.maxLength(255)],
+    img: ['', [Validators.required, regExValidator(URL_REG_EX)]],
+    link: ['', [Validators.required, regExValidator(URL_REG_EX)]],
+    date: ['', [Validators.required, regExValidator(DATE_REG_EX)]],
   });
 
   createNewCard() {
-    console.log('create');
+  }
+
+  createErrorMessage(field: IAdminForm): string | undefined {
+    let message: string | undefined;
+    switch (true) {
+      case !!this.newCard?.get(field.id)?.errors?.['required']:
+        message = field.messageError.required;
+        break;
+      case !!this.newCard?.get(field.id)?.errors?.['minlength']:
+        message = field.messageError.minLength;
+        break;
+      case !!this.newCard?.get(field.id)?.errors?.['maxlength']:
+        message = field.messageError.maxLength;
+        break;
+      case !!this.newCard?.get(field.id)?.errors?.['regEx']:
+        message = field.messageError.pattern;
+        break;
+      default:
+        message = '';
+    }
+    return message;
   }
 }
